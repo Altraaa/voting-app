1d730cefa32227c46c8de68fec998ca8fdb91d09
Altraaa <aldimks7464@gmail.com>
2025-09-14 03:13:20 +0800

    Feature: Revise Footer responsive and add payment process to main layout to exclude navbar and footer for that page

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Twitter, Vote } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="bg-card border-t">
-      <div className="py-12 px-20">
+      <div className="py-12 md:px-20 px-4">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
