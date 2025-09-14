5e6bd929c9ce79871352aeb0d65a3a00dd1f593d
Altraaa <aldimks7464@gmail.com>
2025-09-15 00:10:22 +0800

    Feat: Create all required logic controller

---
+++ src/config/controllers/categoryController.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { categoryService } from "../service/categoryService";
+
+export const categoryController = {
+  async getAll() {
+    const categories = await categoryService.getAll();
+    return NextResponse.json(categories);
+  },
+
+  async create(req: Request) {
+    const { name } = await req.json();
+    const category = await categoryService.create({ name });
+    return NextResponse.json(category);
+  },
+
+  async update(req: Request) {
+    const { id, name } = await req.json();
+    const category = await categoryService.update({ id, name });
+    return NextResponse.json(category);
+  },
+
+  async remove(req: Request) {
+    const { id } = await req.json();
+    await categoryService.remove(id);
+    return NextResponse.json({ message: "Category deleted" });
+  },
+};
