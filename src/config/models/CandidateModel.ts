94013ee02ffdaca6a4cb36c5cd7269f592336f50
Altraaa <aldimks7464@gmail.com>
2025-09-21 02:55:17 +0800

    Fix: Revise candidate logic according to ne Schema

--- src/config/models/CandidateModel.ts
+++ src/config/models/CandidateModel.ts
@@ -3,13 +3,12 @@ import { IVotes } from "./VotesModel";
 
 export interface ICandidate {
   id: string;
-  categoryId: string;
-  votesId: string;
   name: string;
-  description: string;
-  photo_url: string;
+  description?: string; 
+  photo_url?: string;
+  categoryId: string;
+  created: Date;
+  updated: Date;
   category: ICategories;
   votes: IVotes[];
-  created: string;
-  updated: string;
 }
