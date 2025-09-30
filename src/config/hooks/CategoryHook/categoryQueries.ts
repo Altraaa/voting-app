79e7f90a7a863d9abc641a63831bf83241049d8a
Altraaa <aldimks7464@gmail.com>
2025-09-30 17:27:06 +0800

    Fix: Add get By id Hook

--- src/config/hooks/CategoryHook/categoryQueries.ts
+++ src/config/hooks/CategoryHook/categoryQueries.ts
@@ -4,8 +4,17 @@ import { CATEGORY_QUERY_KEYS } from "./categoryQueryKey";
 import { CategoriesRoute } from "@/routes/categoriesRoute";
 
 export const useCategoryQueries = {
-    useGetAllCategories: (filters?: string) =>
-        useApiQuery<CategoriesResponse>(CATEGORY_QUERY_KEYS.list(filters), () =>
-            CategoriesRoute.getAll()
-        ),
-}
+  useGetAllCategories: (filters?: string) =>
+    useApiQuery<CategoriesResponse>(CATEGORY_QUERY_KEYS.list(filters), () =>
+      CategoriesRoute.getAll()
+    ),
+
+  useGetCategoryById: (id: string) =>
+    useApiQuery<CategoriesDetailResponse>(
+      CATEGORY_QUERY_KEYS.detail(id),
+      () => CategoriesRoute.getById(id), 
+      {
+        enabled: !!id,
+      }
+    ),
+};
