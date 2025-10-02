26be7a66178574dd650c4dcab31071432a336a55
Altraaa <aldimks7464@gmail.com>
2025-10-02 19:56:55 +0800

    Fix: Revise type for image and package

--- src/config/types/responseType.ts
+++ src/config/types/responseType.ts
@@ -1,6 +1,7 @@
 import { ICandidate } from "../models/CandidateModel";
 import { ICategories } from "../models/CategoriesModel";
 import { IPackagePurchase } from "../models/PackageHistoryModel";
+import { IPackage } from "../models/PackageModel";
 
 export type CandidateResponse = ICandidate[];
 export type CandidateDetailResponse = ICandidate;
@@ -13,3 +14,6 @@ export type CategoriesDetailResponse = ICategories;
 
 export type PackageHistoryResponse = IPackagePurchase[];
 export type PackageHistoryDetailResponse = IPackagePurchase;
+
+export type PackageResponse = IPackage[];
+export type PackageDetailResponse = IPackage;
