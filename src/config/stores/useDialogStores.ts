30ef744fd62b2bfcff2fe067120689777ac3cb7b
Altraaa <aldimks7464@gmail.com>
2025-10-02 22:16:10 +0800

    Feat: Create store for reusable state

--- src/config/stores/useDialogStores.ts
+++ src/config/stores/useDialogStores.ts
@@ -1,27 +1,5 @@
 import { create } from "zustand";
-
-interface DialogState {
-  isCreateDialogOpen: boolean;
-  isEditDialogOpen: boolean;
-  isDeleteDialogOpen: boolean;
-  isViewDialogOpen: boolean;
-  selectedId: string | null;
-
-  openCreateDialog: () => void;
-  closeCreateDialog: () => void;
-
-  openEditDialog: (id?: string) => void;
-  closeEditDialog: () => void;
-
-  openDeleteDialog: (id?: string) => void;
-  closeDeleteDialog: () => void;
-
-  openViewDialog: (id?: string) => void;
-  closeViewDialog: () => void;
-
-  closeAllDialogs: () => void;
-  setSelectedId: (id: string | null) => void;
-}
+import { DialogState } from "../types/StateType/dialogStateType";
 
 export const useDialogStore = create<DialogState>((set) => ({
   isCreateDialogOpen: false,
