cbc4c9b06976b13fd3246d89495f4623d14138cf
Altraaa <aldimks7464@gmail.com>
2025-10-02 22:13:20 +0800

    Fix: Revise hook for handle simple query

--- src/config/hooks/CategoryHook/categoryQueries.ts
+++ src/config/hooks/CategoryHook/categoryQueries.ts
@@ -1,7 +1,11 @@
 import { useApiQuery } from "@/config/constants/useApiQuery";
-import { CategoriesDetailResponse, CategoriesResponse } from "@/config/types/responseType";
+import {
+  CategoriesDetailResponse,
+  CategoriesResponse,
+} from "@/config/types/responseType";
 import { CATEGORY_QUERY_KEYS } from "./categoryQueryKey";
 import { CategoriesRoute } from "@/routes/categoriesRoute";
+import { ICategories } from "@/config/models/CategoriesModel";
 
 export const useCategoryQueries = {
   useGetAllCategories: (filters?: string) =>
@@ -9,10 +13,15 @@ export const useCategoryQueries = {
       CategoriesRoute.getAll()
     ),
 
+  useGetAllSimpleEvents: (filters?: string) =>
+    useApiQuery<ICategories[]>(CATEGORY_QUERY_KEYS.simpleList(filters), () =>
+      CategoriesRoute.getAllSimple()
+    ),
+
   useGetCategoryById: (id: string) =>
     useApiQuery<CategoriesDetailResponse>(
       CATEGORY_QUERY_KEYS.detail(id),
-      () => CategoriesRoute.getById(id), 
+      () => CategoriesRoute.getById(id),
       {
         enabled: !!id,
       }
