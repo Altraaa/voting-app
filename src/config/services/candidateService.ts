468b8ea718e20a769d7c4f8cbfbac0567f81a43f
Altraaa <aldimks7464@gmail.com>
2025-10-05 18:02:01 +0800

    Feat: Create logic for get Candidate by Category Id

--- src/config/services/candidateService.ts
+++ src/config/services/candidateService.ts
@@ -7,29 +7,98 @@ import {
 export const candidateService = {
   async getAll() {
     return prisma.candidate.findMany({
-      include: { category: true, votes: true },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        photo_url: true,
+        categoryId: true,
+        votes: {
+          select: {
+            userId: true,
+            pointsUsed: true,
+          },
+        },
+      },
     });
   },
 
   async getById(id: string) {
     return prisma.candidate.findUnique({
       where: { id },
-      include: { category: true, votes: true },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        photo_url: true,
+        categoryId: true,
+        votes: {
+          select: {
+            userId: true,
+            pointsUsed: true,
+          },
+        },
+      },
+    });
+  },
+
+  async getByCategorId(categoryId: string) {
+    return prisma.candidate.findMany({
+      where: { categoryId },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        photo_url: true,
+        votes: {
+          select: {
+            userId: true,
+            pointsUsed: true,
+          },
+        },
+      },
     });
   },
 
   async create(data: CandidatesCreatePayload) {
-    return prisma.candidate.create({ data });
+    return prisma.candidate.create({
+      data,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        photo_url: true,
+        categoryId: true,
+      },
+    });
   },
 
   async update(data: CandidatesUpdatePayload) {
     return prisma.candidate.update({
       where: { id: data.id },
-      data: { name: data.name, categoryId: data.categoryId },
+      data: {
+        name: data.name,
+        categoryId: data.categoryId,
+        description: data.description,
+        photo_url: data.photo_url,
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        photo_url: true,
+        categoryId: true,
+      },
     });
   },
 
   async remove(id: string) {
-    return prisma.candidate.delete({ where: { id } });
+    return prisma.candidate.delete({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
   },
 };
