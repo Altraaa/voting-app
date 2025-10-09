a67087823eba88f0283acb77884e0a2511f7caf0
Altraaa <aldimks7464@gmail.com>
2025-10-09 18:51:27 +0800

    Feat: Create new Fitur for OTP

--- src/config/services/otpService.ts
+++ src/config/services/otpService.ts
@@ -1,6 +1,11 @@
 import { prisma } from "@/lib/prisma";
+import { otpPayload } from "../types/otpType";
+import { transporter } from "@/lib/mailer";
+import crypto from "crypto";
 
-export async function verifyOtpService(email: string, otpCode: string) {
+export async function verifyOtpService(data: otpPayload) {
+  const { email, otpCode } = data;
+
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user) throw new Error("USER_NOT_FOUND");
 
@@ -24,4 +29,40 @@ export async function verifyOtpService(email: string, otpCode: string) {
   });
 
   return { success: true, message: "OTP_VERIFIED" };
+}
+
+export async function resendOtpService(email: string) {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) throw new Error("USER_NOT_FOUND");
+
+  if (user.isVerified) {
+    throw new Error("USER_ALREADY_VERIFIED");
+  }
+
+  await prisma.oTPVerification.deleteMany({
+    where: { userId: user.id, verified: false },
+  });
+
+  const otpCode = crypto.randomInt(100000, 999999).toString();
+
+  await prisma.oTPVerification.create({
+    data: {
+      userId: user.id,
+      otpCode,
+      expiresAt: new Date(Date.now() + 60 * 1000), // 1 menit
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Voting App" <${process.env.NEXT_PUBLIC_EMAIL}>`,
+    to: email,
+    subject: "Kode OTP Baru - Voting App",
+    html: `
+      <h2>Hai ${user.name || "User"} 👋</h2>
+      <p>Kode OTP baru kamu adalah: <b>${otpCode}</b></p>
+      <p><i>Kode ini berlaku selama 1 menit.</i></p>
+    `,
+  });
+
+  return { success: true, message: "OTP_RESENT" };
 }
