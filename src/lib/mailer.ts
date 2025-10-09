a917bcf7b3c0e2e93dffb7ea2590b66a28a81cbe
Altraaa <aldimks7464@gmail.com>
2025-10-09 18:58:13 +0800

    Fix: Revise Mailer

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const SMTP_HOST = process.env.NEXT_PUBLIC_SMTP_HOST;
 const SMTP_USER = process.env.NEXT_PUBLIC_EMAIL;
-const SMTP_PASSWORD = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+const SMTP_PASSWORD = process.env.EMAIL_PASSWORD;
 
 export const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
